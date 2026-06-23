90ddb5abbff4abd1d98d17e4fe5d5cc12fd865f2
sarajordanne <diascontaarquivo@gmail.com>
2026-06-23 11:36:42 -0300

    Adicionado atributo salario na classe Funcionario

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -4,10 +4,12 @@ class Funcionario
 {
     private $nome;
     private $matricula;
+    private $salario;
 
-    public function __construct($nome, $matricula)
+    public function __construct($nome, $matricula, $salario)
     {
         $this->nome = $nome;
         $this->matricula = $matricula;
+        $this->salario = $salario;
     }
 }
