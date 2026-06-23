b1f83fa8df8d5c99691a61d86a40a537cd9f2d93
sarajordanne <diascontaarquivo@gmail.com>
2026-06-23 11:53:12 -0300

    Atualização das classes Funcionario e Produto

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -5,11 +5,20 @@ class Funcionario
     private $nome;
     private $matricula;
     private $salario;
+    private $cargo;
 
-    public function __construct($nome, $matricula, $salario)
+    public function __construct($nome, $matricula, $salario, $cargo)
     {
         $this->nome = $nome;
         $this->matricula = $matricula;
         $this->salario = $salario;
+        $this->cargo = $cargo;
+    }
+
+    public function exibirDados()
+    {
+        return "Nome: " . $this->nome .
+               " | Cargo: " . $this->cargo .
+               " | Salário: R$ " . $this->salario;
     }
 }
--- src/Produto.php
+++ src/Produto.php
@@ -4,10 +4,21 @@ class Produto
 {
     private $codigo;
     private $descricao;
+    private $preco;
+    private $quantidade;
 
-    public function __construct($codigo, $descricao)
+    public function __construct($codigo, $descricao, $preco, $quantidade)
     {
         $this->codigo = $codigo;
         $this->descricao = $descricao;
+        $this->preco = $preco;
+        $this->quantidade = $quantidade;
+    }
+
+    public function exibirProduto()
+    {
+        return "Produto: " . $this->descricao .
+               " | Preço: R$ " . $this->preco .
+               " | Estoque: " . $this->quantidade;
     }
 }
